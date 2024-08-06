2816419a5fea895f6579d8dc981a6f24313759ed
Kwenza-Maseko <kwenzaelson80@gmail.com>
2024-08-06 18:03:37 +0200

    second commit

--- app/folder/[[...folder]]/page.tsx
+++ app/folder/[[...folder]]/page.tsx
@@ -13,17 +13,18 @@ const Folder = async () => {
     if (!isAuth) {
         redirect("/sign-in");
     }
+
     return (
         <div className='flex flex-col items-center justify-center mt-8 tht screen'>
             <h1 className='font-bold h1'>You are Interacting as {user?.fullName}</h1>
             <p className="mb-4">Book A slot for your therapy </p>
             {
-                folder_btn.map((item, index) => (
-                    <Link href={`/${item.url}`} passHref>
-                        <div className="buttons mb-3" key={item.id}>
-                            <button
-                                className='btn py-2 px-4 '>
-                                <Image src={item.imageUrl}
+                folder_btn.map((item) => (
+                    <Link href={`/${item.url}`} passHref key={item.id}>
+                        <div className="buttons mb-3">
+                            <button className='btn py-2 px-4 '>
+                                <Image 
+                                    src={item.imageUrl}
                                     width={25}
                                     height={25}
                                     alt='icon'
@@ -36,7 +37,7 @@ const Folder = async () => {
                 ))
             }
         </div>
-    )
+    );
 }
 
-export default Folder
+export default Folder;
