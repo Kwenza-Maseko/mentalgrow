32fd77e660d440543c3e943711fb0f22307ea06c
Kwenza-Maseko <kwenzaelson80@gmail.com>
2024-10-10 12:41:35 +0200

    links

--- components/DashboardLinks.tsx
+++ components/DashboardLinks.tsx
@@ -1,16 +1,16 @@
 'use client'
 import Link from 'next/link'
-import { GiNightSleep } from "@react-icons/all-files/gi/GiNightSleep";
-import { HiEmojiSad } from "@react-icons/all-files/hi/HiEmojiSad";
 import { RiDashboardFill } from "@react-icons/all-files/ri/RiDashboardFill";
-import { GiMeditation } from "@react-icons/all-files/gi/GiMeditation";
+import { FaClipboardList } from "@react-icons/all-files/fa/FaClipboardList";
+import { FaLocationArrow } from "@react-icons/all-files/fa/FaLocationArrow";
+import { MdPerson } from "@react-icons/all-files/md/MdPerson";
 import { usePathname } from 'next/navigation';
 
 const links = [
     { component: RiDashboardFill, link: "/folder", name: "book" },
-    { component: GiMeditation, link: "/folder/waiting", name: "waiting list" },
-    { component: GiNightSleep, link: "/folder/building", name: "Location" },
-    { component: HiEmojiSad, link: "/folder/therapist", name: "therapist" },
+    { component: FaClipboardList, link: "/folder/waiting", name: "waiting list" },
+    { component: FaLocationArrow, link: "/folder/building", name: "Location" },
+    { component: MdPerson, link: "/folder/therapist", name: "therapist" },
 ];
 
 const DashboardLinks = () => {
@@ -25,7 +25,7 @@ const DashboardLinks = () => {
                             <Link href={iconObj.link} passHref key={index}>
                                 <div className="icon-label">
                                     <div className={`flex flex-col gap-1 items-center capitalize`}>
-                                        <div className={`${iconObj.link === path ? 'bg-slate-500 text-white' : 'hover:bg-slate-600 bg-slate-400 hover:text-white'} text-[21px] p-2 rounded-2xl icon-container`}>
+                                        <div className={`${iconObj.link === path ? 'bg-slate-600 text-white' : 'hover:bg-slate-600 bg-slate-400 text-white text-white'} text-[21px] p-2 rounded-2xl icon-container`}>
                                             <IconComponent className={`text-[25px]`} />
                                         </div>
                                         <p className={`${iconObj.link === path ? 'font-semibold text-white' : 'hover:text-white'}`}>{iconObj.name}</p>
